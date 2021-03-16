f5920c4670831215d1464d441af2f81389794dac
Dannkaty <70377119+Dannkaty@users.noreply.github.com>
2021-03-16 09:57:36 +0100

    ajout liens vers pages

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,14 +2,21 @@
 // src/Controller/pageController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+// use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class  ArticleController extends AbstractController
-    {
+    {   
+
+        /**
+         * @Route("/articles/{category}",name="articles")
+         */ 
+         
         public function articles(ArticleRepository $repo, String $category)
         {
        
@@ -24,7 +31,10 @@ class  ArticleController extends AbstractController
                 'articles' => $articles
             ]);
        
-        } 
+        }
+        /**
+         * @Route("/article/{id}",name="category")
+         */ 
         public function article($id){
             $repo = $this->getDoctrine()->getRepository(Article::class);
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,23 +1,86 @@
 <?php
+
 // src/Controller/pageController.php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+
 class  HomeController extends AbstractController
 
+
+// controleur de la page d'acceuil//
 {
-    public function home()
+    /**
+     * @Route("/",name="home")
+     */
+    public function home(): Response
     {
-       
         return $this->render('page/home.html.twig', [
-        'controller_name' => 'page_page/home',
-     ]);
+        'controller_name' => 'HomeController',  
+    ]);
    
+    }
+    
+
+// controleur de la page compte//
+
+     /**
+     * @Route("/compte",name="compte")
+     */  
+     public function compte(): Response
+     {  
+         return $this->render('page/compte.html.twig', [
+         'controller_name' => 'HomeController', 
+
+    ]);
+
     } 
 
-}
+// controleur de la page contact//
+
+     /**
+     * @Route("/contact",name="contact")
+     */ 
+    public function contact(): Response
+    {   
+         return $this->render("page/contact.html.twig", [
+            'controller_name' => 'HomeController', 
+    ]);
+
+    }
+
+// controleur de la page panier//
+
+     /**
+     * @Route("/panier",name="panier")
+     */ 
+    public function panier(): Response
+    {   
+         return $this->render("page/panier.html.twig", [
+            'controller_name' => 'HomeController', 
+    ]);
+
+    }
+
+    // controleur de la page conditionGenerales//
+
+    
+    /**
+     * @Route("/conditions",name="conditions")
+     */  
+    public function conditions(): Response
+    {   
+         return $this->render("page/conditions.html.twig", [
+    
+    ]);
+
+    } 
+
+}   
